261bbcb11057e419fb0ff38b1e396292ba616736
lossp <richard.xxmxx@gmail.com>
2017-06-27 23:35:09 +0800

    jquery-ajax

---
+++ .router.js
@@ -0,0 +1,36 @@
+function setRouter(app){ 
+ var router = app; 
+
+app.get('/getNews', function(req, res){
+    var news = [
+        {
+            title: 'News1'
+        },
+        {
+            title: 'News2'
+        },
+        {
+            title: 'News3'
+        },
+        {
+            title: 'News4'
+        },
+        {
+            title: 'News5'
+        },
+        {
+            title: 'News6'
+        },
+        {
+            title: 'News7'
+        },
+        {
+            title: 'News8'
+        }
+    ];
+    var page = req.query.page;
+    var length = 2;
+    var retnews = news.slice(page*length, page*length+length);
+    res.send(retnews);
+})}
+ module.exports.setRouter = setRouter
